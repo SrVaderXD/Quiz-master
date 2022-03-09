12bfc187e8eccfedf3c08092d7ff75105ade394c
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-03-09 17:24:29 -0300

    Making answers for questions

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,21 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswer;
 
     public string getQuestion()
     {
         return question;
+    }
+
+    public int getCorrectAnswerIndex()
+    {
+        return correctAnswer;
+    }
+
+    public string getCorrectAnswer(int correctIndex)
+    {
+        return answers[correctIndex];
     }
 }
