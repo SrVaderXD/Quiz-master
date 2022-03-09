de10d6c767e594e8b9525aadd85ab11afec3afd8
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-03-09 17:46:50 -0300

    Linking texts with UI

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+
+    async void Start() 
+    {
+        questionText.text = question.getQuestion();
+
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.getCorrectAnswer(i);
+        }
+    }
+}
